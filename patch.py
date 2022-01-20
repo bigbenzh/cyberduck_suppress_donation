a3630f9cf45778e4c478cf2703a04fd2ce55d764
bigbenzh <85002534+bigbenzh@users.noreply.github.com>
2022-01-20 19:09:17 +0800

    Update patch.py

--- patch.py
+++ patch.py
@@ -1,5 +1,11 @@
 import re
 
+
+def replace(string,replace_dict):
+    for key,value in replace_dict.items():
+        string=string.replace(key,value)
+    return string
+
 pattern = re.compile("public static final License EMPTY_LICENSE[\s\S]+(?=};)};\n")
 replace_dict = {"return false":"return true","return null":'return "1"','LocaleFactory.localizedString("Not a valid registration key", "License")':'"patched"','EMPTY_LICENSE':'PATCHED_LICENSE'}
 
@@ -7,12 +13,19 @@ f = open("core/src/main/java/ch/cyberduck/core/aquaticprime/LicenseFactory.java"
 licensefactory=f.read()
 
 EMPTY_LICENSE = pattern.findall(licensefactory)[0]
+PATCHED_LICENSE = replace(EMPTY_LICENSE,replace_dict)
 
-PATCHED_LICENSE = EMPTY_LICENSE
-for key,value in replace_dict.items():
-    PATCHED_LICENSE=PATCHED_LICENSE.replace(key,value)
-
+#order sensitive
 patched = licensefactory.replace(EMPTY_LICENSE,EMPTY_LICENSE+PATCHED_LICENSE).replace("LicenseFactory.EMPTY_LICENSE","LicenseFactory.PATCHED_LICENSE")
+f.seek(0)
+f.write(patched)
+f.close()
+
+replace_dict = {'this.setDefault("update.check", String.valueOf(true));':'this.setDefault("update.check", String.valueOf(false));'}
+f = open("core/src/main/java/ch/cyberduck/core/preferences/Preferences.java","r+",encoding="utf8")
+preferences=f.read()
+patched=replace(preferences,replace_dict)
+
 f.seek(0)
 f.write(patched)
 f.close()
